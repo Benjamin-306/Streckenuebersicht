147bfbb05e1e4f7a052ecf181727c83993ca847b
Benjamin-306 <github@benjamin.fudeus.net>
2026-09-07 17:03:18 +0200

    Add heading

--- src/main/main.py
+++ src/main/main.py
@@ -12,11 +12,16 @@ def main(page: ft.Page):
         page.update() #Aktualisiert Ansicht
 
     # 3. UI Elemente
+    ueberschrift = ft.Container(
+        content = ft.Text(value="Streckenübersicht", size = 32, weight=ft.FontWeight.BOLD),
+        alignment=ft.Alignment(0, -1),
+        expand=True)
+
     text_anzeige = ft.Text(value = "Klicke auf den Button", size = 20)
     mein_button = ft.ElevatedButton(content=ft.Text("Drücken"), on_click=button_klick)
 
     # 4. Elemente hinzfügen
-    page.add(text_anzeige, mein_button)
+    page.add(ueberschrift, text_anzeige, mein_button)
 
 ft.app(target=main)
                                     
