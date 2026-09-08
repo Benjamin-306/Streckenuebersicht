d103a4a361c144da7a0ebb3d166e1fe712063742
Benjamin-306 <github@benjamin.fudeus.net>
2026-09-08 15:04:14 +0200

    Make better to read

--- src/main/main.py
+++ src/main/main.py
@@ -3,11 +3,12 @@ import flet as ft
 def main(page: ft.Page):
     # 1. Fenstertitel + Größe
     page.title = "Streckenübersicht"
+
     page.window.maximized = True
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.START
 
-
+    # Shortcuts
     async def on_keyboard(e: ft.KeyboardEvent):
         if e.key == "Escape":
             await page.window.close()
@@ -17,11 +18,13 @@ def main(page: ft.Page):
     # 3. UI Elemente
     ueberschrift = ft.Container(
         padding = 5,
-        content = ft.Text(value="Streckenübersicht", size = 32, weight=ft.FontWeight.BOLD),
+        content = ft.Text(value="Streckenübersicht", size = 32,
+                          weight=ft.FontWeight.BOLD),
         alignment=ft.Alignment(0, -1),
         height = 150)
     info_text = ft.Container(
-        content = ft.Text(value = "Bitte wähle dein Fortbewegungsmittel:", size = 24, weight=ft.FontWeight.W_500),
+        content = ft.Text(value = "Bitte wähle dein Fortbewegungsmittel:",
+                          size = 24, weight=ft.FontWeight.W_500),
         alignment = ft.Alignment(0, -1),
         height = 50
     )
